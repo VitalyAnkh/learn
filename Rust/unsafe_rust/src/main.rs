44f71ca2cde41b69298814200b6a0fa395c84b00
VitalyR <vitalyankh@gmail.com>
2018-11-29 22:53:29 +0800

    Add unsafe rust

--- Rust/unsafe_rust/src/main.rs
+++ Rust/unsafe_rust/src/main.rs
@@ -1,19 +1,27 @@
 fn main() {
     println!("Hello, world!");
-    let mut num=5;
-    let r1=&num as *const i32;
-    let r2=&mut num as *mut i32;
-    let address =0x012345usize;
-    let r=address as *const i32;
-    unsafe{
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+    let address = 0x012345usize;
+    let r = address as *const i32;
+    unsafe {
         dangerous();
     }
 
     println!("Good luck!");
-    unsafe{
-        println!("r1 is: {}",*r1);
-        println!("r2 is: {}",*r2);
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
     }
+    unsafe {
+        println!("The absolute value of -233 is {}", abs(-233));
+    }
+
+}
+extern "C" {
+    fn abs(input: i32) -> i32;
 }
 
-unsafe fn dangerous(){}
+static mut HELLO_WORLD:&str="Hello, world!";
+unsafe fn dangerous() {}
