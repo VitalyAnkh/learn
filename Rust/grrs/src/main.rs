0fd2dce0d8359c0635eff2e0449a93c306fd8771
VitalyR <vitalyankh@gmail.com>
2018-12-01 20:49:07 +0800

    Add test.txt

--- Rust/grrs/src/main.rs
+++ Rust/grrs/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate structopt;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+
+
+#[derive(StructOpt)]
+struct Cli{
+    pattern:String,
+    #[structopt(parse(from_os_str))]
+    path:std::path::PathBuf,
+
+}
+
+fn main(){
+   let args=Cli::from_args();
+
 }
