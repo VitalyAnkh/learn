d540443f9222ef251a42a1b7f727dafff27ef165
VitalyR <vitalyankh@gmail.com>
2018-12-16 21:15:06 +0800

    Learn haskell module

--- Haskell/module.hs
+++ Haskell/module.hs
@@ -1,3 +1,19 @@
-import Data.Lfist
+import Data.List
+import Data.Char
 numUniques :: (Eq a)=>[a]-> Int
 numUniques = length . nub
+
+search ::(Eq a)=>[a]->[a]-> Bool
+search needle haystack =
+    let nlen = length needle
+    in foldl (\acc x->
+        if take nlen x == needle then True else acc) False (tails haystack)
+
+encode :: Int->String->String
+encode shift str=
+    let ords = map ord str
+        shifted=map (+shift) ords
+    in map chr shifted
+
+decode ::Int->String->String
+decode shift str = encode (negate shift) str
