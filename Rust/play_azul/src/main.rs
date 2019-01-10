8d1a31166e7add0859d35bd106c6e203afe98978
VitalyR <vitalyankh@gmail.com>
2019-01-10 20:48:41 +0800

    To pass exam, I will go to study statistics

--- Rust/play_azul/src/main.rs
+++ Rust/play_azul/src/main.rs
@@ -1,17 +1,31 @@
 extern crate azul;
 
-use azul::prelude::*;
+use azul::{prelude::*, widgets::{label::Label, button::Button}};
 
-struct MyDataModel { }
+struct DataModel {
+    counter: usize,
+}
 
-impl Layout for MyDataModel {
-    fn layout(&self, _: WindowInfo<Self>) -> Dom<Self> {
+impl Layout for DataModel {
+    // Model renders View
+    fn layout(&self, _info: WindowInfo<Self>) -> Dom<Self> {
+        let label = Label::new(format!("{}", self.counter)).dom();
+        let button = Button::with_label("Update counter").dom()
+            .with_callback(On::MouseUp, Callback(update_counter));
+
         Dom::new(NodeType::Div)
+            .with_child(label)
+            .with_child(button)
     }
 }
 
+// View updates Model
+fn update_counter(app_state: &mut AppState<DataModel>, _event: WindowEvent<DataModel>) -> UpdateScreen {
+    app_state.data.modify(|state| state.counter += 1);
+    UpdateScreen::Redraw
+}
+
 fn main() {
-    let app = App::new(MyDataModel { }, AppConfig::default());
-    let window = Window::new(WindowCreateOptions::default(), css::native()).unwrap();
-    app.run(window).unwrap();
+    let app = App::new(DataModel { counter: 0 }, AppConfig::default());
+    app.run(Window::new(WindowCreateOptions::default(), css::native()).unwrap()).unwrap();
 }
