454d000a5eab3ec2e38f94c94756fb79be8e9aa5
VitalyR <vitalyankh@gmail.com>
2019-01-10 20:17:55 +0800

    Play azul

--- Rust/ppc/lab3/src/main.rs
+++ Rust/ppc/lab3/src/main.rs
@@ -50,6 +50,8 @@ fn main() {
     let mut rng=rand::thread_rng();
     let n1:u8=rng.gen();
     let n2:u16=rng.gen();
+    let s="董瑞";
+    println!("{}",s);
     println!("{}",n1);
     println!("{}",rng.gen::<i32>());
 
