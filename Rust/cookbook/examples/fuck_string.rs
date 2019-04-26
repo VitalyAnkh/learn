dc63cc2cb84f5c7d8cafd6bb6a388e61d50f7670
VitalyR <vitalyankh@gmail.com>
2019-04-26 22:30:42 +0800

    learn to accept the reality

---
+++ Rust/cookbook/examples/fuck_string.rs
@@ -0,0 +1,20 @@
+#![feature(core_intrinsics)]
+fn print_type_of<T>(_: &T) {
+    println!("{}", unsafe { std::intrinsics::type_name::<T>() });
+}
+fn main() {
+    let a = String::from("Hello");
+    let b = &a;
+    print_type_of(&&a);
+    print_type_of(&b);
+    print_type_of(&a);
+    let &c = &b;
+}
+// error[E0507]: cannot move out of borrowed content
+//  --> fuck_string.rs:4:9
+//   |
+//   |     let &c = &a;
+//   |         ^-
+//   |         ||
+//   |         |hint: to prevent move, use `ref c` or `ref mut c`
+//   |         cannot move out of borrowed content
