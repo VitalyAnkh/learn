1335cedabcac8a140abf8a1104db4b314f45aae5
VitalyR <vitalyankh@gmail.com>
2019-06-25 22:39:43 +0800

    create bochs-svn package

---
+++ C++/PlayGround/Stock.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vitalyr on 19-6-23.
+//
+
+#include "Stock.h"
---
+++ C++/PlayGround/Stock.h
@@ -0,0 +1,16 @@
+//
+// Created by vitalyr on 19-6-23.
+//
+
+#ifndef PLAYGROUND_STOCK_H
+#define PLAYGROUND_STOCK_H
+
+
+
+class Stock {
+
+};
+
+
+
+#endif //PLAYGROUND_STOCK_H
