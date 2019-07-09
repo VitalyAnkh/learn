94b3fca237f2520e9bff101701583bfbc096cf27
VitalyR <vitalyankh@gmail.com>
2019-07-09 21:52:47 +0800

    Learn concurrency

---
+++ Rust/cookbook/src/bin/adapted_from_dcjanus.rs
@@ -0,0 +1,18 @@
+#![feature(todo_macro)]
+#![feature(result_map_or_else)]
+use std::collections::HashMap;
+
+use failure;
+
+fn main() {}
+
+fn nucleotide_coundts(s: &str) -> Result<HashMap<char, usize>, failure::Error> {
+    let mut result_map:HashMap<char,usize>=HashMap::new();
+    s.chars()
+        .map(|c| counts(c, s) .map_or_else(|_|Err("counts nothing"),|Ok(n)|result_map.insert(c,n).unwrap()))
+        .collect()
+}
+
+fn counts(c: char, dna: &str) -> Result<HashMap<char, usize>, failure::Error> {
+    todo!()
+}
