d673aaeb7c68ac634bff96a2bef3be2f38921bab
VitalyR <vitalyankh@gmail.com>
2019-11-08 20:47:10 +0800

    to compile...

--- Rust/cookbook/src/bin/actix_web_ssl.rs
+++
@@ -1,1 +0,0 @@
-use actix_web::{web,App,HttpResponse,HttpRequest,HttpServer};
--- Rust/cookbook/src/bin/practise_error.rs
+++
@@ -1,9 +0,0 @@
-use std::error::Error;
-use std::fs::File;
-use std::io;
-use std::io::Read;
-fn main() -> Result<String, io::Error> {
-    let mut f = String::new();
-    File::open("foo.txt")?.read_to_string(&mut f)?;
-    Ok(f)
-}
