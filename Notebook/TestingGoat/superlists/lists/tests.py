314128e8c8c8fd83d78da905b8493e1c9b0967ba
VitalyR <vitalyankh@gmail.com>
2019-11-14 17:21:58 +0800

    good start

--- Notebook/TestingGoat/superlists/lists/tests.py
+++ Notebook/TestingGoat/superlists/lists/tests.py
@@ -2,8 +2,12 @@ from django.test import TestCase
 
 # Create your tests here.
 
+from django.urls import resolve
+from django.test import TestCase
+from lists.views import home_page
 
-class SomkeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
--- Notebook/TestingGoat/superlists/lists/views.py
+++ Notebook/TestingGoat/superlists/lists/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def home_page(request):
+    return HttpResponse("Hello World!")
