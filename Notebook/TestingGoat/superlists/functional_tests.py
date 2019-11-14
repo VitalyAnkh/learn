26531945404c84a71e343a883bafeb71f721223a
VitalyR <vitalyankh@gmail.com>
2019-11-14 16:26:02 +0800

    now use unittest

--- Notebook/TestingGoat/superlists/functional_tests.py
+++ Notebook/TestingGoat/superlists/functional_tests.py
@@ -1,9 +1,29 @@
 
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-firefox_dev_binary = FirefoxBinary(
-    '/usr/bin/firefox-developer-edition')
-browser = webdriver.Firefox(firefox_binary=firefox_dev_binary)
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        firefox_dev_binary = FirefoxBinary(
+            '/usr/bin/firefox-developer-edition')
+
+        self.browser = webdriver.Firefox(firefox_binary=firefox_dev_binary)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
 
 browser.get("http:localhost:8000")
-assert "Django" in browser.title
+assert "To-Do" in browser.title
+
+browser.quit()
