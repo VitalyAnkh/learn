16576da5c92c49feeef23abae5854cfa58dc0302
vitalyr <vitalyankh@gmail.com>
2020-07-27 02:27:24 +0800

    update rCore learning

--- OS/rcore-summer-os/src/main.rs
+++ OS/rcore-summer-os/src/main.rs
@@ -2,44 +2,22 @@
 #![no_main]
 #![feature(global_asm)]
 #![feature(llvm_asm)]
+#![feature(panic_info_message)]
+#[macro_use]
+mod console;
+mod panic;
+mod sbi;
+
 use core::panic::PanicInfo;
 
 // - `#![feature(global_asm)]`
 //   内嵌整个汇编文件
 
-
 // 汇编编写的程序入口，具体见该文件
 global_asm!(include_str!("entry.asm"));
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    console_putchar(b'O');
-    console_putchar(b'K');
-    console_putchar(b'\n');
-    console_putchar(b'V');
-    console_putchar(b'R');
-    console_putchar(b'\n');
-    loop {}
-}
-/// 在屏幕上输出一个字符，目前我们先不用了解其实现原理
-pub fn console_putchar(ch: u8) {
-    let _ret: usize;
-    let arg0: usize = ch as usize;
-    let arg1: usize = 0;
-    let arg2: usize = 0;
-    let which: usize = 1;
-    unsafe {
-        llvm_asm!("ecall"
-             : "={x10}" (_ret)
-             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
-             : "memory"
-             : "volatile"
-        );
-    }
+    println!("Hello rCore-Tutorial!");
+    panic!("end of rust_main")
 }
--- OS/rcore-summer-os/src/sbi.rs
+++ OS/rcore-summer-os/src/sbi.rs
@@ -18,11 +18,10 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let ret;
     unsafe {
         llvm_asm!("ecall"
-                  : "={x10} (ret)"
-                  : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "x17" (which)
+                  : "={x10}" (ret)
+                  : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
                   : "memory" // 加入 memory 选项
-                  : "volatile" // 说明这些内存访问是volatile 的，防止编译器做激进的优化（例如调换指令顺序等破坏 SBI 调用行为的优化
-        );
+                  : "volatile"); // 说明这些内存访问是volatile 的，防止编译器做激进的优化（例如调换指令顺序等破坏 SBI 调用行为的优化
     }
     ret
 }
