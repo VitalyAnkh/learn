b05ba4da554e2704ad16ed9af83d1d60bc249e90
vitalyr <vitalyankh@gmail.com>
2020-07-27 06:05:44 +0800

    update rCore learning

---
+++ OS/rcore-summer-os/src/interrupt/handler.rs
@@ -0,0 +1,73 @@
+use super::context::Context;
+use super::timer;
+use riscv::register::{
+    scause::{Exception, Interrupt, Scause, Trap},
+    sie, stvec,
+};
+
+global_asm!(include_str!("./interrupt.asm"));
+
+/// 初始化中断处理
+///
+/// 把中断入口 `__interrupt` 写入 `stvec` 中，并且开启中断使能
+pub fn init() {
+    unsafe {
+        extern "C" {
+            /// `interrupt_asm` 的中断入口
+            fn __interrupt();
+        }
+        // 使用 Direct 模式，将中断入口设置为 `__interrupt`
+        stvec::write(__interrupt as usize, stvec::TrapMode::Direct);
+    }
+}
+
+/// 中断的处理入口
+///
+/// `interrupt.asm` 首先保存寄存器至 Context ，将其作为参数和scause 以及 stval 一并传入此函数
+/// 具体的中断类型需要根据 scause 来推断，然后分别处理
+#[no_mangle]
+pub fn handle_interrupt(context: &mut Context, scause: Scause, stval: usize) {
+    // 可以通过 Debug 来查看发生了什么中断
+    // println!("{:?}", context.scause.cause());
+    match scause.cause() {
+        // 断点中断
+        Trap::Exception(Exception::Breakpoint) => breakpoint(context),
+        // 时钟中断
+        Trap::Interrupt(Interrupt::SupervisorTimer) => supervisor_timer(context),
+        // 捕获 LoadFault
+        Trap::Exception(Exception::LoadFault) => {
+            if stval == 0x0 {
+                println!("SUCCESS!");
+            }
+            panic!("access memory error!");
+        }
+        // 其他情况，终止该线程
+        _ => fault(context, scause, stval),
+    }
+}
+
+/// 处理 ebreak 断点
+///
+/// 继续执行，其中 `sepc` 增加 2 字节， 以跳过当前这条 `ebreak` 指令
+fn breakpoint(context: &mut Context) {
+    println!("Breakpoint at 0x{:x}", context.sepc);
+    context.sepc += 2;
+    // context.sepc = 0;
+}
+
+/// 处理时钟中断
+///
+/// 目前只会在 [`timer`] 模块中进行计数
+fn supervisor_timer(_: &Context) {
+    timer::tick();
+}
+
+/// 出现未能解决的异常
+fn fault(context: &mut Context, scause: Scause, stval: usize) {
+    panic! {
+        "Unresolved interrupt: {:?}\n{:?}\nstval: {:x}",
+        scause.cause(),
+        context,
+        stval
+    }
+}
--- OS/rcore-summer-os/src/main.rs
+++ OS/rcore-summer-os/src/main.rs
@@ -5,19 +5,20 @@
 #![feature(panic_info_message)]
 #[macro_use]
 mod console;
+mod interrupt;
 mod panic;
 mod sbi;
 
-use core::panic::PanicInfo;
-
 // - `#![feature(global_asm)]`
 //   内嵌整个汇编文件
-
 // 汇编编写的程序入口，具体见该文件
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
-pub extern "C" fn rust_main() -> ! {
-    println!("Hello rCore-Tutorial!");
-    panic!("end of rust_main")
+pub extern "C" fn rust_main() {
+    // 初始化各种模块
+    interrupt::init();
+    println!("Hello rCore Tutorial!");
+
+    unsafe { llvm_asm!("ebreak"::::"volatile") }
 }
