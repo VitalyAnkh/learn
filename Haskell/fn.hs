52f6bc6063ea2c889a6d827f561bde0692dac31c
vitalyr <vitalyankh@gmail.com>
2021-10-12 06:18:51 +0800

    New life!

--- Haskell/fn.hs
+++ Haskell/fn.hs
@@ -43,32 +43,33 @@ tail' xs = case xs of
 -- listToTree (x1:x2:xs) = Node (Node Empty x1 Empty) x2 (listToTree xs)
 
 data Tree a = Nil | Leaf a | Node (Tree a) (Tree a)
- deriving Show
-
+  deriving (Show)
 
 {-+------------------+--
 --+--Helper Funtion--+--
 --+------------------+-}
 toTree [] _ _ str
-    | str == "" = Nil
-    | otherwise = Node (Leaf str) Nil
-toTree ('[':xs) currentLevel treeLevel str
-    | currentLevel==treeLevel = Node gs hs
-    | otherwise = toTree xs currentLevel treeLevel str
-        where gs = toTree xs (currentLevel+1)  (treeLevel+1) str
-              hs = toTree xs (currentLevel)  (treeLevel+1) str
-toTree (',':xs) currentLevel treeLevel str
-    | currentLevel==treeLevel = if (str=="") then hs else (Node gs hs)
-    | otherwise = toTree xs currentLevel treeLevel ""
-        where gs = Leaf str
-              hs = toTree xs currentLevel treeLevel ""
-toTree (']':xs) currentLevel treeLevel str
-    | currentLevel == treeLevel = Node gs hs
-    | otherwise = toTree xs (currentLevel) (treeLevel-1) ""
-        where gs = Leaf str
-              hs = Nil
-toTree (x:xs) currentLevel treeLevel str = toTree xs currentLevel treeLevel (str++[x])
-
+  | str == "" = Nil
+  | otherwise = Node (Leaf str) Nil
+toTree ('[' : xs) currentLevel treeLevel str
+  | currentLevel == treeLevel = Node gs hs
+  | otherwise = toTree xs currentLevel treeLevel str
+  where
+    gs = toTree xs (currentLevel + 1) (treeLevel + 1) str
+    hs = toTree xs (currentLevel) (treeLevel + 1) str
+toTree (',' : xs) currentLevel treeLevel str
+  | currentLevel == treeLevel = if (str == "") then hs else (Node gs hs)
+  | otherwise = toTree xs currentLevel treeLevel ""
+  where
+    gs = Leaf str
+    hs = toTree xs currentLevel treeLevel ""
+toTree (']' : xs) currentLevel treeLevel str
+  | currentLevel == treeLevel = Node gs hs
+  | otherwise = toTree xs (currentLevel) (treeLevel - 1) ""
+  where
+    gs = Leaf str
+    hs = Nil
+toTree (x : xs) currentLevel treeLevel str = toTree xs currentLevel treeLevel (str ++ [x])
 
 {-+----------------------------------+--
 --+-Check if two trees are identical-+--
@@ -76,13 +77,12 @@ toTree (x:xs) currentLevel treeLevel str = toTree xs currentLevel treeLevel (str
 areIdentical Nil Nil = True
 areIdentical Nil _ = False
 areIdentical _ Nil = False
-areIdentical (Node xs ys) (Node xs' ys')= (areIdentical xs xs') && (areIdentical ys ys')
+areIdentical (Node xs ys) (Node xs' ys') = (areIdentical xs xs') && (areIdentical ys ys')
 areIdentical (Node xs ys) _ = False
 areIdentical _ (Node xs' ys') = False
 areIdentical (Leaf a) (Leaf b)
- | a==b = True
- | otherwise = False
-
+  | a == b = True
+  | otherwise = False
 
 {-+------------------+--
 --+-Get left subtree-+--
@@ -98,57 +98,54 @@ right (Node t s) = s
 right (Leaf a) = Nil
 right Nil = Nil
 
-
 {-+------------------+--
 --+-Check if subtree-+--
 --+------------------+-}
 isSubtree Nil Nil = True
 isSubtree Nil _ = False
 isSubtree t s = if gs then gs else ((isSubtree (left t) s) || (isSubtree (right t) s))
-    where gs = areIdentical t s
-
+  where
+    gs = areIdentical t s
 
 {-+-----------------------------------------+--
 --+-Check if subtree and retrieve functions-+--
 --+-to obtain this subtree form main tree---+--
 --+-----------------------------------------+-}
-isSubtreeFn Nil Nil f = (True,f)
-isSubtreeFn Nil _ _ = (False,["id"])
+isSubtreeFn Nil Nil f = (True, f)
+isSubtreeFn Nil _ _ = (False, ["id"])
 isSubtreeFn t s f
-    | gs == True = (gs,f)
-    | ((fst as)==True && (fst bs)==True) = (True,(snd as)++(snd bs))
-    | (fst as) == True = as
-    | (fst bs) == True = bs
-    | otherwise = (False,["id"])
-    where gs = areIdentical t s
-          as = isSubtreeFn (left t) s (map ("head."++) f)
-          bs = isSubtreeFn (right t) s (map ("tail."++) f)
-
+  | gs == True = (gs, f)
+  | ((fst as) == True && (fst bs) == True) = (True, (snd as) ++ (snd bs))
+  | (fst as) == True = as
+  | (fst bs) == True = bs
+  | otherwise = (False, ["id"])
+  where
+    gs = areIdentical t s
+    as = isSubtreeFn (left t) s (map ("head." ++) f)
+    bs = isSubtreeFn (right t) s (map ("tail." ++) f)
 
 {-+====================================================================================+--
 --+----------------------------------List Functions------------------------------------+--
 --+====================================================================================+-}
 
-
 {-+------------------+--
 --+-Any data to tree-+--
 --+------------------+-}
 elemToTree a = Leaf p
- where p = show a
-
+  where
+    p = show a
 
 {-+------------------+--
 --+-Any list to tree-+--
 --+------------------+-}
 listToTree l = toTree (p (show l)) 0 0 ""
- where p = init.tail                        ----to remove first and last square braces
-
+  where
+    p = init . tail ----to remove first and last square braces
 
 {-+------------------+--
 --+-Check if subtree-+--
 --+------------------+-}
 isSublist xs ys = isSubtree (listToTree xs) (listToTree ys)
-
 
 {-+----------------------+--
 --+-Check if subtree and-+--
--- Notebook/apue/ch3/hole.c
+++ Notebook/apue/ch3/hole.c
@@ -7,13 +7,7 @@ char buf2[] = "ABCDEFGHIJ";
 int main() {
   int fd;
   if ((fd = creat("file.hole", FILE_MODE)) < 0)
-    err_sys("creat error");
-  if (write(fd, buf1, 10) != 10)
-    err_sys("buf1 write error");
-  // offset now is 10
-  if (lseek(fd, 16384, SEEK_SET) == -1)
-    err_sys("lseek error");
-  // offset now = 16384
+    err_sys(
 
   if (write(fd, buf2, 10) != 10)
     err_sys("buf2 write error");
