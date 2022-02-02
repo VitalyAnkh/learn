8d0ccd39094a1e614ad139212a0a64170c54ed78
vitalyr <vitalyankh@gmail.com>
2022-02-02 22:24:15 +0800

    Use python script to delete repos on github

---
+++ Python/delete-github-repo.py
@@ -0,0 +1,21 @@
+from time import sleep
+import requests
+
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": "token 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",  # 填入token
+    "X-OAuth-Scopes": "repo",
+}
+
+with open("./repos.txt", "r", encoding="utf-8") as f:
+    data = f.readlines()
+
+url = "https://api.github.com/repos/{}/{}"
+urls = []
+for line in data:
+    name, repo = line.strip().split("/")
+    urls.append(url.format(name, repo))
+
+for url in urls:
+    requests.delete(url=url, headers=headers)
+    print("{} is deleted".format(url))
